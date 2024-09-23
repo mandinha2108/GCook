9f745aa1d03344620f5473fba4c1d7b79250a0bf
mandinha2108 <amafur8@gmail.com>
2024-09-23 12:22:28 -0300

    continuação gCook ( registroVM )

--- GCook/ViewModels/RegistroVM.cs
+++ GCook/ViewModels/RegistroVM.cs
@@ -1,12 +1,26 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
 
 namespace GCook.ViewModels
 {
     public class RegistroVM
     {
-        
+      [Display (Name = "Nome Completo", Prompt = "Informe seu Nome Completo")]
+      [Required(ErrorMessage = "Por favor, informe seu Nome")]
+      [StringLength(60, ErrorMessage = "O Nome deve possuir no máximo 60 caracteres")]
+      public string Nome { get; set; }
+
+
+      [DataType(DataType.Date)]
+      [Display(Name = "Data de Nascimento", Prompt = "Informe sua Data de Nascimento")]
+      [Required(ErrorMessage = "Por favor, informe sua Data de Nascimento")]
+      public DateTime? DataNascimento { get; set; } = null;
+
+      [Display(Prompt = "Informe seu Email")]
+      []
     }
 }
